f3ecab0a07e01010e296cd4d7665c6df1a973071
tygroo <tygroo972@free.fr>
2013-10-28 14:23:00 +0100

    upgrade racine

---
+++ src/talkatives/GreetingService.java
@@ -0,0 +1,18 @@
+package talkatives;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/greeting")
+public class GreetingService {
+    @GET
+    public String message() {
+        return "Hi REST!";
+    }
+
+    @POST
+    public String lowerCase(final String message) {
+        return "Hi REST!".toLowerCase();
+    }
+}
