97c0b83b0fa10336ac95dcc66e376b1a47e7e280
tygroo <tygroo972@free.fr>
2014-01-07 11:45:41 +0100

    commit wo bdd

--- src/main/java/epsi/talkative/resource/EditorsResource.java
+++ src/main/java/epsi/talkative/resource/EditorsResource.java
@@ -1,6 +1,6 @@
 package epsi.talkative.resource;
 
-import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.WebApplicationException;
@@ -11,7 +11,7 @@ import epsi.talkative.repository.EditorRepository;
 @Path("editors")
 public class EditorsResource {
 
-	@EJB
+	@Inject
 	private EditorRepository editorRepository;
 
 	@Path("{editor}")
--- src/test/java/epsi/talkative/resource/ArticlesResourceTest.java
+++ src/test/java/epsi/talkative/resource/ArticlesResourceTest.java
@@ -1,41 +1,24 @@
 package epsi.talkative.resource;
 
-import java.util.Properties;
-
-import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.UserTransaction;
-
 import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
 import org.apache.cxf.jaxrs.client.ClientConfiguration;
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.openejb.jee.EjbJar;
 import org.apache.openejb.jee.WebApp;
-import org.apache.openejb.jee.jpa.unit.PersistenceUnit;
 import org.apache.openejb.junit.ApplicationComposer;
 import org.apache.openejb.testing.Classes;
-import org.apache.openejb.testing.Configuration;
 import org.apache.openejb.testing.EnableServices;
 import org.apache.openejb.testing.Module;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import epsi.talkative.repository.Editor;
-import epsi.talkative.repository.EditorRepository;
+import epsi.talkative.repository.MockEditorRepository;
 
 @RunWith(ApplicationComposer.class)
 @EnableServices("jaxrs")
 public class ArticlesResourceTest {
-
-	@Resource
-	private UserTransaction userTransaction;
-
-	@PersistenceContext(unitName = "talkative-persistence")
-	private EntityManager entityManager;
 
 	@Module
 	@Classes(TalkativeApplication.class)
@@ -44,42 +27,16 @@ public class ArticlesResourceTest {
 	}
 
 	@Module
-	@Classes(EditorRepository.class)
+	@Classes(MockEditorRepository.class)
 	public EjbJar ejb() {
 		return new EjbJar();
 	}
 
-	@Module
-	public PersistenceUnit persistenceUnit() {
-		return new PersistenceUnit("talkative-persistence");
-	}
-
-	@Configuration
-	public Properties properties() throws Exception {
-		Properties properties = new Properties();
-		properties.put("javax.persistence.jdbc.driver", "org.hsqldb.jdbcDriver");
-		properties.put("javax.persistence.jdbc.url", "jdbc:hsqldb:mem");
-		properties.put("openjpa.jdbc.SynchronizeMappings", "buildSchema(ForeignKeys=true)");
-		properties.put("openjpa.Log", "DefaultLevel=WARN, Runtime=INFO, Tool=INFO, SQL=TRACE");
-		return properties;
-	}
-
-	@Before
-	public void clearDatabase() throws Exception {
-		userTransaction.begin();
-		entityManager.createNativeQuery("truncate schema public and commit").executeUpdate();
-		userTransaction.commit();
-	}
-
 	@Test
-	public void canRetrieveNoCommentForNewArticle() throws Exception {
-		userTransaction.begin();
-		Editor editor = createEditor();
-		userTransaction.commit();
-
+	public void canRetrieveNoCommentForNewArticle() {
 		WebClient client = createWebClient();
 
-		String message = client.path("editors").path(editor.getId()).path("articles/www.epsi.fr/i4/mon%20article.html/comments").get(String.class);
+		String message = client.path("editors/davidg/articles/www.epsi.fr/i4/mon%20article.html/comments").get(String.class);
 
 		Assert.assertEquals(204, client.getResponse().getStatus());
 		Assert.assertEquals("http://www.epsi.fr/i4/mon%20article.html; rel=\"article\"", client.getResponse().getMetadata().getFirst("Link"));
@@ -90,7 +47,7 @@ public class ArticlesResourceTest {
 	public void cannotRetrieveCommentWhenEditorIsNotKnown() {
 		WebClient client = createWebClient();
 
-		client.path("editors").path("unknown").path("articles/www.epsi.fr/i4/myarticle.html/comments").get();
+		client.path("editors").path(MockEditorRepository.UNKNOWN_EDITOR).path("articles/www.epsi.fr/i4/myarticle.html/comments").get();
 
 		Assert.assertEquals(403, client.getResponse().getStatus());
 	}
@@ -101,12 +58,5 @@ public class ArticlesResourceTest {
 		config.getInInterceptors().add(new LoggingInInterceptor());
 		config.getOutInterceptors().add(new LoggingOutInterceptor());
 		return client;
-	}
-
-	private Editor createEditor() {
-		Editor editor = new Editor();
-		editor.setId("myEditor");
-		entityManager.persist(editor);
-		return editor;
 	}
 }
